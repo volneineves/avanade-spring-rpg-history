5490c648aafc3c01c4453dc978d0e6aab4c36f1b
Volnei Neves <volneinevesfilho@hotmail.com>
2023-09-14 23:22:09 -0300

    feat(entities): Updating tables for schemmas

--- src/main/java/com/avanade/history/entities/HistoryBattle.java
+++ src/main/java/com/avanade/history/entities/HistoryBattle.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Entity
-@Table(name = "history_battles")
+@Table(name = "history_battles", schema = "log")
 public class HistoryBattle {
 
     @Id
--- src/main/java/com/avanade/history/entities/HistoryTurn.java
+++ src/main/java/com/avanade/history/entities/HistoryTurn.java
@@ -7,7 +7,7 @@ import java.util.Date;
 import java.util.UUID;
 
 @Entity
-@Table(name = "history_turns")
+@Table(name = "history_turns", schema = "log")
 public class HistoryTurn {
 
     @Id
